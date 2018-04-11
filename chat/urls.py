5e983c42ffbb13c9a3b1b8272b72a36f4700b6b8
Neville Oronni <nevooronni@gmail.com>
2018-04-11 04:59:14 +0300

    initial commit

---
+++ chat/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    # URL form : "/api/messages/1/2"
+    path('api/messages/<int:sender>/<int:receiver>', views.message_list, name='message-detail'), # For GET request.
+    # URL form : "/api/messages/"
+    path('api/messages/', views.message_list, name='message-list'), # For POST
+    # URL form "/api/users/1"
+    path('api/users/<int:pk>', views.user_list, name='user-detail'), # GET request for user with id
+    path('api/users/', views.user_list, name='user-list'), # POST for new user and GET for all users list
+    path('', views.index, name='index'),
+    path('register', views.register_view, name='register'),
+]
